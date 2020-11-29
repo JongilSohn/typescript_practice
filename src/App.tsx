11063140f61b62a5e43ae1b46583e5b01bad847a
JongilSohn <sji7532@gmail.com>
2020-11-29 21:05:26 +0900

    TypeScript에서의 Reducer 사용 방법

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import Counter from './Counter';
+import React from "react";
+import ReducerSample from "./ReducerSample";
 
 function App() {
-  return (
-    <Counter />
-  );
+  return <ReducerSample />;
 }
 
 export default App;
